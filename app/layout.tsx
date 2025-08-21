f7310668e86a8fabf904049b0670f630bb688c1a
Saidul147 <sidul.islam.146@gmail.com>
2025-08-21 15:56:03 +0600

    accessibie added and its working on bbsfullstacks-projects/eric-weisbrot-frontend-tghh

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
-"use client"
-// import type { Metadata } from "next";
+
+import type { Metadata } from "next";
 import "./globals.css";
 import { AppConfig } from "@/config/app.config";
 import TopBar from "@/components/Shared/layouts/TopBar";
@@ -8,10 +8,10 @@ import Footer from "@/components/Shared/layouts/Footer";
 import AccessiBeWidget from "@/components/Widget/accessibe-widget";
 import Script from "next/script";
 
-// export const metadata: Metadata = {
-//   title: AppConfig().app.name,
-//   description: AppConfig().app.slogan,
-// };
+export const metadata: Metadata = {
+  title: AppConfig().app.name,
+  description: AppConfig().app.slogan,
+};
 
 export default function RootLayout({
   children,
@@ -24,20 +24,8 @@ export default function RootLayout({
         <TopBar/>
         <Navbar />
         {children}
-        <div className="border p-10">
           <AccessiBeWidget licenseKey={process.env.NEXT_PUBLIC_ACCESSIBE_LICENSE_KEY} />
-
-        </div>
         <Footer />
-          {/* <Script
-          strategy="afterInteractive" // Ensures the script is loaded after the page is interactive
-          src="https://acsbapp.com/apps/app/dist/js/app.js"
-          onLoad={() => {
-            if (typeof window !== 'undefined' && window.acsbJS) {
-              window.acsbJS.init();
-            }
-          }}
-        /> */}
       </body>
     </html>
   );
