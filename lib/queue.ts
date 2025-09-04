cb6ee3aa51e4b425388408b7631232a6c223d82a
Foysal Hasan <foysal3909@gmail.com>
2025-09-04 09:00:03 +0600

    redis updated

--- lib/queue.ts
+++ lib/queue.ts
@@ -4,20 +4,25 @@ import { sendEmail } from './sendEmail'; // Import your email sending logic
 
 const host = process.env.REDIS_HOST || 'localhost';
 const port = Number(process.env.REDIS_PORT) || 6379;
+const password = process.env.REDIS_PASSWORD || '';
 
 // Create a BullMQ queue for email tasks
 const emailQueue = new Queue('emailQueue', {
   connection: {
     host: host,
     port: port, // Redis connection settings
+    password,
   },
 });
 
 // Create a scheduler to manage the job failures, retries, etc.
 const emailQueueScheduler = new Queue('emailQueue', {
   connection: {
-    host: 'localhost',
-    port: 6379,
+    // host: 'localhost',
+    // port: 6379,
+    host: host,
+    port: port, // Redis connection settings
+    password,
   },
 });
 
@@ -33,15 +38,21 @@ const emailWorker = new Worker(
     if (result.success) {
       console.log(`Email sent successfully for ${formType} form.`);
     } else {
-      console.error(`Error sending email for ${formType} form:`, result.message);
+      console.error(
+        `Error sending email for ${formType} form:`,
+        result.message,
+      );
     }
   },
   {
     connection: {
-      host: 'localhost',
-      port: 6379,
+      // host: 'localhost',
+      // port: 6379,
+      host: host,
+      port: port, // Redis connection settings
+      password,
     },
-  }
+  },
 );
 
 export { emailQueue, emailQueueScheduler, emailWorker };
