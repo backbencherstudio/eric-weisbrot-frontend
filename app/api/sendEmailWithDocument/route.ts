82bb20b6bd21bc34b487711a2e62813037a0e343
Foysal Hasan <foysal3909@gmail.com>
2025-11-21 16:17:50 +0600

    increase payload size

--- app/api/sendEmailWithDocument/route.ts
+++ app/api/sendEmailWithDocument/route.ts
@@ -1,6 +1,13 @@
-import { NextRequest, NextResponse } from 'next/server';
-import { sendEmail } from '@/lib/sendEmail';
+import { NextRequest, NextResponse } from "next/server";
+import { sendEmail } from "@/lib/sendEmail";
 
+export const config = {
+  api: {
+    bodyParser: {
+      sizeLimit: "20mb", // adjust the size you need
+    },
+  },
+};
 // Handle the image upload and additional fields
 export async function POST(req: NextRequest) {
   try {
@@ -8,19 +15,25 @@ export async function POST(req: NextRequest) {
     const formData = await req.formData();
 
     // Extract the image file and form fields from the form data
-    const file = formData.get('document');
-    const firstName = formData.get('firstName');
-    const lastName = formData.get('lastName');
-    const phone = formData.get('phone');
-    const email = formData.get('email');
+    const file = formData.get("document");
+    const firstName = formData.get("firstName");
+    const lastName = formData.get("lastName");
+    const phone = formData.get("phone");
+    const email = formData.get("email");
 
     // Validate if the file and form data are present
     if (!file || !(file instanceof Blob)) {
-      return NextResponse.json({ success: false, message: 'No image file uploaded' }, { status: 400 });
+      return NextResponse.json(
+        { success: false, message: "No image file uploaded" },
+        { status: 400 }
+      );
     }
 
-    if (!firstName || !phone ) {
-      return NextResponse.json({ success: false, message: 'Missing form data' }, { status: 400 });
+    if (!firstName || !phone) {
+      return NextResponse.json(
+        { success: false, message: "Missing form data" },
+        { status: 400 }
+      );
     }
 
     const buffer = Buffer.from(await file.arrayBuffer());
@@ -38,16 +51,19 @@ export async function POST(req: NextRequest) {
           mimetype: file.type,
         },
       },
-      'documentUpload',
+      "documentUpload"
     );
 
     if (!result.success) {
-      return NextResponse.json({ success: false, message: result.message }, { status: 500 });
+      return NextResponse.json(
+        { success: false, message: result.message },
+        { status: 500 }
+      );
     }
 
     return NextResponse.json({
       success: true,
-      message: 'Document emailed successfully',
+      message: "Document emailed successfully",
       firstName,
       lastName,
       phone,
@@ -55,6 +71,9 @@ export async function POST(req: NextRequest) {
     });
   } catch (error: any) {
     // Handle unexpected errors
-    return NextResponse.json({ success: false, message: error.message }, { status: 500 });
+    return NextResponse.json(
+      { success: false, message: error.message },
+      { status: 500 }
+    );
   }
 }
