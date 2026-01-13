b00834cef6913fda699c7d64b45bbf8e535ebbb0
saidul147 <sidul.islam.146@gmail.com>
2026-01-13 11:17:52 +0600

    toast success message show successfullu

--- components/ContactUs/ContactUsForm.tsx
+++ components/ContactUs/ContactUsForm.tsx
@@ -77,7 +77,7 @@ export default function ContactUsForm() {
       const result = await response?.json()
 
       if (result?.success) {
-       toast.success("Appointment request submitted")
+       toast.success("Form submitted Sucessfully")
       } else {
         toast.error("Form submission failed.")
       }
--- components/RequestAppointment/RequestAppointmentForm.tsx
+++ components/RequestAppointment/RequestAppointmentForm.tsx
@@ -89,7 +89,7 @@ export default function RequestAppointmentForm() {
          const result = await response?.json()
    
          if (result?.success) {
-           toast.success("Form submitted Successfully")
+           toast.success("Appointment request submitted")
          } else {
             toast.error("Form submission failed.")
          }
